6d032390769ea511fa5abd8969fca7b297f21706
Joao Gab <santosjoaogabriel09@gmail.com>
2023-07-01 11:09:18 -0300

    Optional Chaining

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
-// Object
+// Optional Chaining
 
 const user = {
   name: 'Diego',
-  age: 27,
+  age:  27,
   address: {
-    street: 'Street Teste',
-    number: 176,
+    // street: 'St. Teste',
+    number: 177,
+    zip: {
+      code: '49075470',
+      city: 'Aracaju'
+    },
+    // showFullAddress() {
+    //   return 'ok';
+    // }
   }
-};
-
-document.body.innerText = Object.keys(user) // Return all keys of object
-document.body.innerText = JSON.stringify(Object.values(user)) // Return all values of object
-document.body.innerText = JSON.stringify(Object.entries(user)) // Return vector include keys and values of object
-
-// In ( Verifica se tem alguma coisa dentro do objeto )
-//
-
-// Desestruturação
-
-function showAge({ age: Idade }) {
-  return Idade
 }
 
-const { address, age: Age, nickname = 'Defina seu nickname'  } = user
-// document.body.innerText = JSON.stringify({address, Age, nickname}) 
-document.body.innerText = showAge(user)
+const key = 'street';
 
-// Rest Operator
-// const { name, ...rest } = user
-
-const array = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-]
-
-const [first, , third,...rest] = array
-
-document.body.innerText = JSON.stringify({ first, third, rest })
+document.body.innerText = user.address?.[key] ?? "A"
+document.body.innerText = user.address?.showFullAddress?.() ?? "Não informado"
